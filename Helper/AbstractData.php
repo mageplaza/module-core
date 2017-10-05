34d3a957dc6d9d1a35d22e3c6f90f2935ba64663
haitv282 <haitv282@gmail.com>
2017-10-05 16:29:07 +0700

    compatible with 2.2.0 (serialize/unserialize)

--- Helper/AbstractData.php
+++ Helper/AbstractData.php
@@ -34,91 +34,91 @@ use Magento\Store\Model\ScopeInterface;
  */
 class AbstractData extends AbstractHelper
 {
-	/**
-	 * @type array
-	 */
-	protected $_data = [];
+    /**
+     * @type array
+     */
+    protected $_data = [];
 
-	/**
-	 * @type \Magento\Store\Model\StoreManagerInterface
-	 */
-	protected $storeManager;
+    /**
+     * @type \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
 
-	/**
-	 * @type \Magento\Framework\ObjectManagerInterface
-	 */
-	protected $objectManager;
+    /**
+     * @type \Magento\Framework\ObjectManagerInterface
+     */
+    protected $objectManager;
 
     /**
      * @var \Magento\Framework\Json\Helper\Data
      */
     protected static $_jsonHelper;
 
-	/**
-	 * @param \Magento\Framework\App\Helper\Context $context
-	 * @param \Magento\Framework\ObjectManagerInterface $objectManager
-	 * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-	 */
-	public function __construct(
-		Context $context,
-		ObjectManagerInterface $objectManager,
-		StoreManagerInterface $storeManager
-	)
-	{
-		$this->objectManager = $objectManager;
-		$this->storeManager  = $storeManager;
+    /**
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        Context $context,
+        ObjectManagerInterface $objectManager,
+        StoreManagerInterface $storeManager
+    )
+    {
+        $this->objectManager = $objectManager;
+        $this->storeManager  = $storeManager;
 
-		parent::__construct($context);
-	}
+        parent::__construct($context);
+    }
 
-	/**
-	 * @param $field
-	 * @param null $storeId
-	 * @return mixed
-	 */
-	public function getConfigValue($field, $storeId = null)
-	{
-		return $this->scopeConfig->getValue(
-			$field,
-			ScopeInterface::SCOPE_STORE,
-			$storeId
-		);
-	}
+    /**
+     * @param $field
+     * @param null $storeId
+     * @return mixed
+     */
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
 
-	/**
-	 * @param $name
-	 * @param $value
-	 * @return $this
-	 */
-	public function setData($name, $value)
-	{
-		$this->_data[$name] = $value;
+    /**
+     * @param $name
+     * @param $value
+     * @return $this
+     */
+    public function setData($name, $value)
+    {
+        $this->_data[$name] = $value;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @param $name
-	 * @return null
-	 */
-	public function getData($name)
-	{
-		if (array_key_exists($name, $this->_data)) {
-			return $this->_data[$name];
-		}
+    /**
+     * @param $name
+     * @return null
+     */
+    public function getData($name)
+    {
+        if (array_key_exists($name, $this->_data)) {
+            return $this->_data[$name];
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getCurrentUrl()
-	{
-		$model = $this->objectManager->get('Magento\Framework\UrlInterface');
+    /**
+     * @return mixed
+     */
+    public function getCurrentUrl()
+    {
+        $model = $this->objectManager->get('Magento\Framework\UrlInterface');
 
-		return $model->getCurrentUrl();
-	}
+        return $model->getCurrentUrl();
+    }
 
     /**
      * @return \Magento\Framework\Json\Helper\Data|mixed
@@ -167,22 +167,61 @@ class AbstractData extends AbstractHelper
         return $decodeValue;
     }
 
-	/**
-	 * @param $path
-	 * @param array $arguments
-	 * @return mixed
-	 */
-	public function createObject($path, $arguments = [])
-	{
-		return $this->objectManager->create($path, $arguments);
-	}
 
-	/**
-	 * @param $path
-	 * @return mixed
-	 */
-	public function getObject($path)
-	{
-		return $this->objectManager->get($path);
-	}
+    /**
+     * @param $data
+     * @return string
+     */
+    public function serialize($data)
+    {
+        if ($this->versionCompare('2.2.0')) {
+            return self::jsonEncode($data);
+        }
+
+        return serialize($data);
+    }
+
+    /**
+     * @param $string
+     * @return mixed
+     */
+    public function unserialize($string)
+    {
+        if ($this->versionCompare('2.2.0')) {
+            return self::jsonDecode($string);
+        }
+
+        return unserialize($string);
+    }
+
+    /**
+     * @param $ver
+     * @return mixed
+     */
+    public function versionCompare($ver)
+    {
+        $productMetadata = $this->objectManager->get(\Magento\Framework\App\ProductMetadataInterface::class);
+        $version         = $productMetadata->getVersion(); //will return the magento version
+
+        return version_compare($version, $ver, '>=');
+    }
+
+    /**
+     * @param $path
+     * @param array $arguments
+     * @return mixed
+     */
+    public function createObject($path, $arguments = [])
+    {
+        return $this->objectManager->create($path, $arguments);
+    }
+
+    /**
+     * @param $path
+     * @return mixed
+     */
+    public function getObject($path)
+    {
+        return $this->objectManager->get($path);
+    }
 }
