61814c498124cbe8a16de1349f764d1d8230f32a
Jimmy <jimmy@mageplaza.com>
2018-08-10 09:12:46 +0700

    Update help.js

--- view/adminhtml/web/js/help.js
+++ view/adminhtml/web/js/help.js
@@ -1,12 +1,13 @@
 
 var mpHelpDb = {
-	1: {
-		'path': 'admin/system_config/index',
-		'css_selector': '#general_single_store_mode_enabled',
-		'type': 'link',
-		'text': 'How to enable Single Store Mode, {link}.',
-		'url': 'https://www.mageplaza.com/kb/how-enable-single-store-mode-magento-2.html',
-		'anchor': 'learn more',
+	'admin/system_config/index' : {
+		1: {
+			'css_selector': '#general_single_store_mode_enabled',
+			'type': 'link',
+			'text': 'How to enable Single Store Mode, {link}.',
+			'url': 'https://www.mageplaza.com/kb/how-enable-single-store-mode-magento-2.html',
+			'anchor': 'learn more',
+		}
 	}
 }
 
@@ -25,10 +26,11 @@ var url = window.location.href;
 
 for (var key in mpHelpDb){
 	data = mpHelpDb[key];
-	if (mpHelpDb.hasOwnProperty(key) && url.includes(data.path)) {
-		var html = buildHtml(data);
-
-		jQuery(html).insertAfter(data.css_selector);
+	if (mpHelpDb.hasOwnProperty(key) && url.includes(key){
+	    	for (var m in data){
+			var html = buildHtml(data[m]);
+			jQuery(html).insertAfter(data[m].css_selector);
+		}
 	}
 }
 
